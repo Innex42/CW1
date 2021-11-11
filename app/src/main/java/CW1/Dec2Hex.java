5b3aa04c87e3dc038131c1914f77e443f4db642c
Innex42 <innex4@gmail.com>
2021-11-11 21:29:50 +0000

    refactored and improved the testing

--- app/src/main/java/CW1/Dec2Hex.java
+++ app/src/main/java/CW1/Dec2Hex.java
@@ -6,29 +6,21 @@ class Dec2Hex
 
     public static int Arg1;
 
-    public static boolean ArrayError=false;
-    public static boolean NumberError=false;
     public static String convert(String args[]){
 	 try{
 
         Arg1 = Integer.parseInt(args[0]);
+}
 
-        }catch(ArrayIndexOutOfBoundsException e){System.out.println("A number was not entered"); ArrayError=true;}
-
-        catch(NumberFormatException e){System.out.println("That was an invalid input"); NumberError=true;}
+	catch(Exception e){System.out.println("Invaild input"); return "-1";}
+	
 
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
 	String hexadecimal="";
-        if (ArrayError==true){
-                System.out.println("Please try again with a valid number");
-		return "-1";
-        }else if  (NumberError==true){
-                System.out.println("Please try again with a valid number");
-		return "-1";
-        }else{
         num = Arg1;
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
+	System.out.println(num);
         if (num==0){
 		return "0";
 	}else if (num <=-1){
@@ -39,7 +31,7 @@ class Dec2Hex
             hexadecimal= ch[rem] + hexadecimal;
             num= num/16;
         }
-}
+
 	return hexadecimal;
         
 	}
--- app/src/test/java/CW1/Dec2HexTest.java
+++ app/src/test/java/CW1/Dec2HexTest.java
@@ -9,7 +9,8 @@ import static org.junit.Assert.*;
 public class Dec2HexTest {
     @Test public void ValidateConverstion() {
         assertEquals("F", Dec2Hex.convert(new String[]{"15"}));
-	assertEquals("-1", Dec2Hex.convert(new String[]{}));
         assertEquals("0", Dec2Hex.convert(new String[]{"0"}));
+	assertEquals("-1",Dec2Hex.convert(new String[]{""}));
+	assertEquals("-1",Dec2Hex.convert(new String[]{"words"}));
     }
 }
