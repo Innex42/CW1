a09736e48898f4bc1313b0a661a3c08cfbaa4d31
Innex42 <innex4@gmail.com>
2021-11-11 21:47:47 +0000

    Formatted code

--- app/src/main/java/CW1/Dec2Hex.java
+++ app/src/main/java/CW1/Dec2Hex.java
@@ -7,12 +7,14 @@ class Dec2Hex
     public static int Arg1;
 
     public static String convert(String args[]){
-	 try{
+	try {
 
-        Arg1 = Integer.parseInt(args[0]);
-}
+        	Arg1 = Integer.parseInt(args[0]);
+	}
 
-	catch(Exception e){ return "-1";}
+	catch (Exception e) {
+		return "-1";
+	}
 	
 
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
@@ -20,13 +22,12 @@ class Dec2Hex
 	String hexadecimal="";
         num = Arg1;
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
-	System.out.println(num);
         if (num==0){
 		return "0";
-	}else if (num <=-1){
-		return "-1";}
-	while(num != 0)
-        {
+	} else if (num <=-1) {
+		return "-1";
+	}
+	while(num != 0) {
             rem=num%16;
             hexadecimal= ch[rem] + hexadecimal;
             num= num/16;
@@ -34,11 +35,10 @@ class Dec2Hex
 
 	return hexadecimal;
         
-	}
-    public static void main(String args[])
-    {
+     }
+     public static void main(String args[]) {
 	String Result = convert(args);
-	if (Result=="-1"){
+	if (Result.equals("-1")){
 		System.out.println("Invalid Input");
 		return;}
 	System.out.println("Hexadecimal representation is: " + Result);
